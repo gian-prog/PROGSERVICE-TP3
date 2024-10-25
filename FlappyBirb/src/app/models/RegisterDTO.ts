de37ea9a941f00c36eb19b317d080b4820f1d1d5
6262557 <6262557@LLBINFD070422>
2024-10-25 14:57:58 -0400

    Progress: Commencement Tests avec Angular

---
+++ FlappyBirb/src/app/models/RegisterDTO.ts
@@ -0,0 +1,7 @@
+export class RegisterDTO{
+    constructor(
+        public username : string,
+        public email : string,
+        public password: string,
+        public passwordConfirm : string){}
+}
